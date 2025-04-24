c4f57b3dbb56316cfcb1ea0bf2dcab385a3f0351
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-04-24 22:01:06 +0530

    create another component

--- hello-angular/src/app/app.component.ts
+++ hello-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent], // import Login component
+  imports: [RouterOutlet, LoginComponent, NavbarComponent], // import Login component and Navbar component
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
