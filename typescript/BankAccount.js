cd3336242b28ef6bb807e653d0565d8851d839b9
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-04-25 22:38:28 +0530

    class methods

--- typescript/BankAccount.js
+++ typescript/BankAccount.js
@@ -1,5 +1,5 @@
 var BankAccount = /** @class */ (function () {
-    function BankAccount(ownerName, accountNumber, balance) {
+    function BankAccount(ownerName, accountNumber, balance, isSaving) {
         this.ownerName = ownerName;
         this.accountNumber = accountNumber;
         this.balance = balance;
@@ -7,9 +7,24 @@ var BankAccount = /** @class */ (function () {
     BankAccount.prototype.displayData = function () {
         console.log("owner name: ".concat(this.ownerName, ", account number: ").concat(this.accountNumber, ", balance: ").concat(this.balance));
     };
+    BankAccount.prototype.depositMoney = function (amount) {
+        this.balance += amount;
+    };
+    BankAccount.prototype.withdrawMoney = function (amount) {
+        this.balance -= amount;
+    };
+    BankAccount.prototype.getBalance = function () {
+        return this.balance;
+    };
+    BankAccount.prototype.getAccountNumber = function () {
+        return this.accountNumber;
+    };
     return BankAccount;
 }());
-var emp1 = new BankAccount('Kamal', '34-070-810', 30400);
-emp1.displayData();
-/*-------------- OUTPUT ----------- */
-// owner name: Kamal, account number: 34-070-810, balance: 30400
+var acc1 = new BankAccount('Kamal', '34-070-810', 30400, true);
+acc1.displayData(); // owner name: Kamal, account number: 34-070-810, balance: 30400
+acc1.depositMoney(20000);
+acc1.withdrawMoney(5000);
+var balance = acc1.getBalance();
+console.log(balance);
+console.log(acc1.getAccountNumber());
--- typescript/BankAccount.ts
+++ typescript/BankAccount.ts
@@ -1,14 +1,37 @@
 class BankAccount {
-    constructor(public ownerName: string, protected accountNumber: string, private balance: number) { }
+    constructor(public ownerName: string, protected accountNumber: string, private balance: number, isSaving: boolean) { }
 
     displayData() {
         console.log(`owner name: ${this.ownerName}, account number: ${this.accountNumber}, balance: ${this.balance}`);
     }
 
+    depositMoney(amount: number): void {
+        this.balance += amount;
+    }
+
+    withdrawMoney(amount: number): void {
+        this.balance -= amount;
+    }
+
+    getBalance(): number {
+        return this.balance;
+    }
+
+    getAccountNumber() {
+        return this.accountNumber;
+    }
+
 }
 
-let emp1 = new BankAccount('Kamal', '34-070-810', 30400);
-emp1.displayData();
+let acc1 = new BankAccount('Kamal', '34-070-810', 30400, true);
 
-/*-------------- OUTPUT ----------- */
-// owner name: Kamal, account number: 34-070-810, balance: 30400
+acc1.displayData();  // owner name: Kamal, account number: 34-070-810, balance: 30400
+
+acc1.depositMoney(20000);
+
+acc1.withdrawMoney(5000);
+
+let balance = acc1.getBalance();
+console.log(balance); // 45400
+
+console.log(acc1.getAccountNumber()); // 34-070-810
