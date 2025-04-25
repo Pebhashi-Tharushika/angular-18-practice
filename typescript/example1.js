c4337c22f2844d60b05a819e7c981fc5eb9717de
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-04-25 08:12:59 +0530

    let, var, const keyword

--- typescript/example1.js
+++ typescript/example1.js
@@ -9,3 +9,18 @@ var country = "Japan";
 var age = 20;
 var weight = 63.25;
 var isMarried = false;
+/* let(new kewword) , var(old keyword) , const */
+if (true) {
+    var m = "Amal";
+    var n = 6;
+}
+// console.log(m); // error: Cannot find name 'm'
+console.log(n);
+function printUserDetails() {
+    var r = "Amal";
+    var t = 6;
+}
+// console.log(r); // error: Cannot find name 'r'
+// console.log(t); // Cannot find name 't'
+var pi = 3.14;
+// pi = 3; // error - Cannot assign to 'pi' because it is a constant
--- typescript/example1.ts
+++ typescript/example1.ts
@@ -14,3 +14,23 @@ let country: string = "Japan";
 let age: number = 20;
 let weight: number = 63.25;
 let isMarried: boolean = false;
+
+
+/* let(new kewword) , var(old keyword) , const */
+
+if (true) {
+    let m: string = "Amal";
+    var n: number = 6;
+}
+// console.log(m); // error: Cannot find name 'm'
+console.log(n);
+
+function printUserDetails() {
+    let r: string = "Amal";
+    var t: number = 6;
+}
+// console.log(r); // error: Cannot find name 'r'
+// console.log(t); // Cannot find name 't'
+
+const pi = 3.14; 
+// pi = 3; // error - Cannot assign to 'pi' because it is a constant
