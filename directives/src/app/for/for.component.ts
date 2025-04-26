40f18ea54e1135af684f18b9aee4a292e22540a0
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-04-26 13:38:34 +0530

    @empty with @for

--- directives/src/app/for/for.component.ts
+++ directives/src/app/for/for.component.ts
@@ -1,15 +1,26 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-for',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,NgIf],
   templateUrl: './for.component.html',
   styleUrl: './for.component.css'
 })
 export class ForComponent {
 
-  students : string[] = ['Amal', 'Kamal', 'Nimal', 'Wimal', 'Bimal'];
+  students: string[] = ['Amal', 'Kamal', 'Nimal', 'Wimal', 'Bimal'];
 
+  users: User[] = [{ id: 1, name: 'user1', email: 'user1@gmail.com' },
+                   { id: 2, name: 'user2', email: 'user2@gmail.com' },
+                   { id: 3, name: 'user3', email: 'user3@gmail.com' }
+                  ];
+
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
 }
