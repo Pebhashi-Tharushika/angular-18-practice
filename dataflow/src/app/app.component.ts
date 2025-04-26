2f1a94dd63241b4c16f96e5c896ae260246cf748
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-04-26 19:24:19 +0530

    data flow from child to parent using @Output

--- dataflow/src/app/app.component.ts
+++ dataflow/src/app/app.component.ts
@@ -9,20 +9,25 @@ import { UserComponent } from './user/user.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements AfterViewInit { // implement by one of the component lifecycle hook and import it
+export class AppComponent /*implements AfterViewInit*/ { // implement by one of the component lifecycle hook and import it
   title = 'dataflow';
 
-  @ViewChild(UserComponent) userComponent!: UserComponent; // ! - non-null assertion operator
+  // @ViewChild(UserComponent) userComponent!: UserComponent; // ! - non-null assertion operator
 
   message: string = "";
 
-  constructor() {
-    console.log(this.userComponent); //undefined
-  }
+  // constructor() {
+  //   console.log(this.userComponent); //undefined
+  // }
 
   //runs once after component's view and child views are initialized
-  ngAfterViewInit(): void {
-    console.log(this.userComponent); // UserComponent object
-    this.message = this.userComponent.userMessage;
+  // ngAfterViewInit(): void {
+  //   console.log(this.userComponent); // UserComponent object
+  //   this.message = this.userComponent.userMessage;
+  // }
+
+  handleDataFromChild(message: string) {
+    console.log(message);
+    this.message = message;
   }
 }
--- dataflow/src/app/user/user.component.ts
+++ dataflow/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,5 +12,15 @@ export class UserComponent {
   userMessage: string = "This is child message";
 
   userName: string = "Amal";
+
+  private salary: number = 25000;
+
+  protected age: number = 13;
+
+  @Output() dataEmitter = new EventEmitter<string>();// This is an event
+
+  sendData() {
+    this.dataEmitter.emit(this.userMessage); // publish userMessage to this dataEmitter event
+  }
 
 }
