59004fc6b25e06f0e0196b200ff350778fccf1c1
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-04-26 19:06:08 +0530

    data flow from child to parent using @ViewChild

--- dataflow/src/app/app.component.ts
+++ dataflow/src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { PostListComponent } from './post-list/post-list.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PostListComponent], // import PostListComponent
+  imports: [PostListComponent, UserComponent], // import UserComponent
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit { // implement by one of the component lifecycle hook and import it
   title = 'dataflow';
+
+  @ViewChild(UserComponent) userComponent!: UserComponent; // ! - non-null assertion operator
+
+  message: string = "";
+
+  constructor() {
+    console.log(this.userComponent); //undefined
+  }
+
+  //runs once after component's view and child views are initialized
+  ngAfterViewInit(): void {
+    console.log(this.userComponent); // UserComponent object
+    this.message = this.userComponent.userMessage;
+  }
 }
