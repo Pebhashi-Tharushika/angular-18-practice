7b0cf99e8c4a0f71fc5f209fefbc8ad764d681c1
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-04-26 14:26:32 +0530

    loop context variable: index, first, last, odd, even

--- directives/src/app/for/for.component.ts
+++ directives/src/app/for/for.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-for',
   standalone: true,
-  imports: [NgFor,NgIf],
+  imports: [NgFor, NgIf],
   templateUrl: './for.component.html',
   styleUrl: './for.component.css'
 })
@@ -13,13 +13,35 @@ export class ForComponent {
   students: string[] = ['Amal', 'Kamal', 'Nimal', 'Wimal', 'Bimal'];
 
   users: User[] = [{ id: 1, name: 'user1', email: 'user1@gmail.com' },
-                   { id: 2, name: 'user2', email: 'user2@gmail.com' },
-                   { id: 3, name: 'user3', email: 'user3@gmail.com' }
+                  { id: 2, name: 'user2', email: 'user2@gmail.com' },
+                  { id: 3, name: 'user3', email: 'user3@gmail.com' }
                   ];
+
+  customers: Customer[] = [];
+
+  count: number = this.customers.length;
+
+  fruits: string[] = ['Apple', 'Mango', 'Grapes', 'Banana', 'Cherry'];
+
+  addNewCustomer() {
+    const customer: Customer = {
+      id: this.count + 1,
+      name: `customer${this.count + 1}`,
+      email: `customer${this.count + 1}@gmail.com`
+    };
+    this.customers.push(customer);
+    this.count++;
+  }
 
 }
 
 interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface Customer {
   id: number;
   name: string;
   email: string;
