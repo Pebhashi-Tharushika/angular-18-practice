836c3693ce9d9231f26adb242891ac6084c37872
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-04-27 12:59:59 +0530

    buil-in pipe - string manipulation pipe

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { UpperCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, DecimalPipe, JsonPipe, LowerCasePipe, PercentPipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe, LowerCasePipe, TitleCasePipe, DecimalPipe, CurrencyPipe, PercentPipe, DatePipe, JsonPipe, SlicePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pipes';
+  
+}
+
+interface User {
+  name: string;
+  age: number;
 }
