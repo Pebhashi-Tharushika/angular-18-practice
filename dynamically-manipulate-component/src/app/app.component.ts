f4b1bdfe47616f9c53c5d4211ac55473ff80c5a9
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-04-27 11:49:49 +0530

    render component dynamically - ngComponentOutlet

--- dynamically-manipulate-component/src/app/app.component.ts
+++ dynamically-manipulate-component/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { UserComponent } from './user/user.component';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UserComponent],
+  imports: [UserComponent, NgComponentOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dynamically-manipulate-component';
+
+  loadUserComponent() {
+    return UserComponent;
+  }
 }
