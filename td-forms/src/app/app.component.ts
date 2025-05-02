f53ccec4e2b3aa1b77d8fc9acebc72ef4f69afb0
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-02 16:45:43 +0530

    access input values in a template driven form using @ViewChild

--- td-forms/src/app/app.component.ts
+++ td-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms'; // import NgForm
 
 @Component({
   selector: 'app-root',
@@ -9,22 +9,34 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  @ViewChild('f') form !: NgForm
+
   title = 'td-forms';
 
-  onSubmit(event: any) {
-    // event.preventDefault();
+  onSubmit() {
     console.group("Form submitted");
-    console.log(event); //ngModel object
-    console.log(event.value.name);
-    console.log(event.value.email);
-    console.log(event.value.address);
-    
-    // console.log(event.target[0].value);
-    // console.log(event.target[1].value);
-    // console.log(event.target[2].value);
 
-    // console.log(event.target.name.value);
-    // console.log(event.target.email.value);
-    // console.log(event.target.address.value);
+    console.log(this.form);
+    console.log(this.form.value.name);
+    console.log(this.form.value.email);
+    console.log(this.form.value.address);
   }
+
+  // onSubmit(event: any) {
+  //   // event.preventDefault();
+  //   console.group("Form submitted");
+  //   console.log(event); //ngModel object
+  //   console.log(event.value.name);
+  //   console.log(event.value.email);
+  //   console.log(event.value.address);
+
+  //   // console.log(event.target[0].value);
+  //   // console.log(event.target[1].value);
+  //   // console.log(event.target[2].value);
+
+  //   // console.log(event.target.name.value);
+  //   // console.log(event.target.email.value);
+  //   // console.log(event.target.address.value);
+  // }
 }
