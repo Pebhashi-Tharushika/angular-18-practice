14d9ee8d213b8d8709031edb98b9ae6618e41ef8
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-02 14:15:20 +0530

    access input using event

--- td-forms/src/app/app.component.ts
+++ td-forms/src/app/app.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'td-forms';
 
-  onSubmit(){
+  onSubmit(event: any) {
+    event.preventDefault();
     console.group("Form submitted");
+    console.log(event);
+    console.log(event.target[0].value);
+    console.log(event.target[1].value);
+    console.log(event.target[2].value);
   }
 }
