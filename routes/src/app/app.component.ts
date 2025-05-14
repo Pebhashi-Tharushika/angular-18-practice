091324771d93ff0adb7b381cc06edf888f1c7c77
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-14 10:49:35 +0530

    routerLinkActive and exact URL match using roterLinkActiveOptions

--- routes/src/app/app.component.ts
+++ routes/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { ThirdComponent } from './third/third.component';
@@ -8,7 +8,7 @@ import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FirstComponent, SecondComponent, ThirdComponent, HomeComponent, RouterLink],
+  imports: [RouterOutlet,FirstComponent, SecondComponent, ThirdComponent, HomeComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
