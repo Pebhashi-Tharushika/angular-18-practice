8715bc355977000cc39f015edb4a12dd52ad9d96
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-14 10:55:47 +0530

    programmatically navigation

--- routes/src/app/second/second.component.ts
+++ routes/src/app/second/second.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-second',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './second.component.html',
   styleUrl: './second.component.css'
 })
 export class SecondComponent {
+
+  constructor(private router: Router) { }
+
+  navigateToThird() {
+    // this.router.navigate(['/third']);
+    this.router.navigateByUrl('/third');
+  }
 
 }
