714810c4d2837572d7c0a52195d40eeaa86cc5b9
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-14 14:33:15 +0530

    set path parameter, query parameter and fragment in ts file

--- routes/src/app/product/product.component.ts
+++ routes/src/app/product/product.component.ts
@@ -1,12 +1,12 @@
 import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, RouterLink } from '@angular/router';
+import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
   // Import RouterLink for navigation & Import JsonPipe to display JSON data in the template
-  imports: [JsonPipe, RouterLink], 
+  imports: [JsonPipe, RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
@@ -14,7 +14,7 @@ export class ProductComponent implements OnInit {
 
   product!: Product;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     // this.product = {
@@ -22,14 +22,21 @@ export class ProductComponent implements OnInit {
     //   name: this.activatedRoute.snapshot.params['name']
     // }
 
-    this.activatedRoute.params.subscribe((data:Params) => {
+    this.activatedRoute.params.subscribe((data: Params) => {
       this.product = {
         id: data['id'],
         name: data['name']
       }
     });
   }
-  
+
+  loadSampleProduct() { 
+    this.router.navigate(['/product', 1, 'abc'], { // path parameters
+      queryParams: {page:1, search:'qwer'},        // query parameters
+      fragment: 'loading'                         // fragment
+    });
+    // this.router.navigateByUrl('/product/1/abc');
+  }
 }
 
 interface Product {
