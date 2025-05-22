b288632ed111249f9aa90d6db7fbf9b826965ccf
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-22 18:12:47 +0530

    create, subsribe and unsubscribe custom observable

--- rx-js/src/app/home/home.component.ts
+++ rx-js/src/app/home/home.component.ts
@@ -9,20 +9,40 @@ import { interval, Observable, Subscription } from 'rxjs';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  
+
   intervalSubscription !: Subscription;
 
-  // Creates an Observable that emits sequential and incremental numbers every specified interval of time 
   ngOnInit(): void {
-    // subscribe
-    this.intervalSubscription = interval(1000).subscribe((val) => {
-      console.log(val);
+
+    let customObservable = new Observable(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count); //Observable notify / emit value to observers via next() method
+        count++;
+      },1000);
     });
+
+    this.intervalSubscription = customObservable.subscribe(count => {
+      console.log(count);
+    })
   }
 
   ngOnDestroy(): void {
-    // unsubscribe
-    this.intervalSubscription.unsubscribe();
+   this.intervalSubscription.unsubscribe();
   }
+  
+
+  // Creates an Observable that emits sequential and incremental numbers every specified interval of time 
+  // ngOnInit(): void {
+  //   // subscribe
+  //   this.intervalSubscription = interval(1000).subscribe((val) => {
+  //     console.log(val);
+  //   });
+  // }
+
+  // ngOnDestroy(): void {
+  //   // unsubscribe
+  //   this.intervalSubscription.unsubscribe();
+  // }
 
 }
