548c857776360b67c38971f2e8bb881fb7e46b8d
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-27 11:59:46 +0530

    not allowed writing to signal inside effect()

--- signals/src/app/effects/effects.component.ts
+++ signals/src/app/effects/effects.component.ts
@@ -11,13 +11,17 @@ export class EffectsComponent {
 
   count = signal(0);
 
-  constructor(){
+  a = signal(0);
+
+  constructor() {
     effect(() => {
-      console.log('Effect triggered: count changed to', this.count());
+      // console.log('Effect triggered: count changed to', this.count());
+      // this.a.set(2);
+      // this.a.set(this.count() * 2);
     });
   }
 
-  updatecount(){
+  updatecount() {
     this.count.update(value => value + 1);
   }
 
