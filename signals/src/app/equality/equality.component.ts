77623b78deb3499fb1f736eee5d321a53ef7e1fa
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-27 15:34:31 +0530

    equality function with computed()

--- signals/src/app/equality/equality.component.ts
+++ signals/src/app/equality/equality.component.ts
@@ -1,33 +1,38 @@
-import { Component, effect, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
 import { isEqual } from 'lodash';
 
 @Component({
   selector: 'app-equality',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './equality.component.html',
   styleUrl: './equality.component.css'
 })
-export class EqualityComponent {
+export class EqualityComponent implements OnInit {
 
   // data = signal({ name: 'John', age: 30 },{
   //   equal: (a, b) => a.name === b.name && a.age === b.age
   // });
 
-  data = signal({ name: 'John', age: 30 },{
-    equal: isEqual
+  data = signal({ name: 'John', age: 30 }, { equal: isEqual });
+
+  computedData = computed(() => {
+    console.log('executing the computed signal');
+    return this.data();
   });
 
   constructor() {
-    effect(() => {
-      console.log('Executing the effect');
-      console.log("Data changed", this.data());
-    });
+    // effect(() => {
+    //   console.log('Executing the effect');
+    //   console.log("Data changed", this.data());
+    // });
   }
 
   ngOnInit() {
     setTimeout(() => {
-      this.data.set({ name: 'John', age: 30 }); 
+      console.log('executed the setTimeout');
+      this.data.set({ name: 'John', age: 30 });
     }, 4000);
   }
 }
