576699b0bffa79276a9aea73afb9b82f1bec1c02
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-28 09:06:59 +0530

    untracked()

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CounterComponent } from './counter/counter.component';
 import { EffectsComponent } from './effects/effects.component';
 import { EqualityComponent } from './equality/equality.component';
+import { UntrackedComponent } from './untracked/untracked.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CounterComponent, EffectsComponent, EqualityComponent],
+  imports: [CounterComponent, EffectsComponent, EqualityComponent, UntrackedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ signals/src/app/untracked/untracked.component.ts
@@ -0,0 +1,31 @@
+import { Component, effect, OnInit, signal, untracked } from '@angular/core';
+
+@Component({
+  selector: 'app-untracked',
+  standalone: true,
+  imports: [],
+  templateUrl: './untracked.component.html',
+  styleUrl: './untracked.component.css'
+})
+export class UntrackedComponent implements OnInit {
+
+  count = signal(0);
+  name = signal('Angular');
+
+  constructor() {
+    effect(() => {
+      console.log(`hello ${this.name()}. count is ${untracked(this.count)}`);
+    })
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('count is triggered');
+      this.count.set(3);
+    }, 3000);
+    setTimeout(() => {
+      console.log('name is triggered');
+      this.name.set('Angular 18');
+    }, 6000);
+  }
+}
