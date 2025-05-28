c74b5674bc6902495a7c230c821f822e1b9890b7
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-28 15:52:39 +0530

    optional and required signal inputs

---
+++ signals/src/app/signal-inputs/signal-inputs.component.ts
@@ -0,0 +1,16 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-inputs',
+  standalone: true,
+  imports: [],
+  templateUrl: './signal-inputs.component.html',
+  styleUrl: './signal-inputs.component.css'
+})
+export class SignalInputsComponent {
+
+  firstName = input.required<string>();  // required signal input
+  lastName = input<string>();            // optional signal input
+  age = input();                        // optional signal input with default value
+
+}
