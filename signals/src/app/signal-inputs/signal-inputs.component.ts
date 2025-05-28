1ff52d2dfe788ad45829fb6bb90e37e137f6ab21
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-28 16:30:48 +0530

    value transform of signal inputs

--- signals/src/app/signal-inputs/signal-inputs.component.ts
+++ signals/src/app/signal-inputs/signal-inputs.component.ts
@@ -9,16 +9,29 @@ import { Component, computed, effect, input } from '@angular/core';
 })
 export class SignalInputsComponent {
 
-  firstName = input.required<string>();  // required signal input
-  lastName = input<string>();            // optional signal input
-  age = input(0, { alias: 'agevalue' });     // optional signal input with default value
+  // firstName = input.required<string>();  // required signal input
+  // lastName = input<string>();            // optional signal input
+  // age = input(0, { alias: 'agevalue' });     // optional signal input with default value
 
-  ageMultiplied = computed(() => this.age() * 5);
+  // ageMultiplied = computed(() => this.age() * 5);
 
-  constructor(){
-    effect(()=> {
-      console.log(this.firstName());
-    })
+  // constructor(){
+  //   effect(()=> {
+  //     console.log(this.firstName());
+  //   })
+  // }
+
+  sample = input(false, {
+    transform: (value: boolean | string) => {
+      console.log('Transform: ', typeof (value));
+      return typeof value === 'string' ? value === '' : value;
+    }
+  });
+
+  constructor() {
+    effect(() => {
+      console.log(this.sample());
+    });
   }
 
 }
