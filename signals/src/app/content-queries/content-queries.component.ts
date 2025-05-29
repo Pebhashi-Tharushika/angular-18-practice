68e9d3fcb7c82ca8e491fcb2ee0ac416087a4cdc
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-29 14:27:44 +0530

    content queries - contentChildren()

--- signals/src/app/content-queries/content-queries.component.ts
+++ signals/src/app/content-queries/content-queries.component.ts
@@ -1,4 +1,4 @@
-import { Component, contentChild, effect, ElementRef } from '@angular/core';
+import { Component, contentChild, contentChildren, effect, ElementRef } from '@angular/core';
 import { WidgetComponent } from '../widget/widget.component';
 
 @Component({
@@ -10,12 +10,19 @@ import { WidgetComponent } from '../widget/widget.component';
 })
 export class ContentQueriesComponent {
 
-  h1 = contentChild<ElementRef>('h2');
+  // `h1` is a signal that holds a reference to a single projected HTML element from the parent template.
+  h1 = contentChild<ElementRef>('h1');
+
+  // `h2` is a signal that holds references to multiple projected HTML elements from the parent template.
+  h2 = contentChildren<ElementRef>('h2');
+
+  // `widget` is a signal that holds a reference to a projected another child component from the parent template.
   widget = contentChild(WidgetComponent);
 
   constructor() {
     effect(() => {
       console.log(this.h1());
+      console.log(this.h2());
       console.log(this.widget());
     });
   }
