ad9b87b70feac0886e0689cad57c38934fe26891
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-29 10:39:43 +0530

    view queries - viewChild()

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal, viewChild } from '@angular/core';
 import { CounterComponent } from './counter/counter.component';
 import { EffectsComponent } from './effects/effects.component';
 import { EqualityComponent } from './equality/equality.component';
@@ -8,18 +8,30 @@ import { RxjsInteropComponent } from './rxjs-interop/rxjs-interop.component';
 import { ToObservableComponent } from './to-observable/to-observable.component';
 import { SignalInputsComponent } from './signal-inputs/signal-inputs.component';
 import { ModelInputsComponent } from './model-inputs/model-inputs.component';
+import { ViewQueriesComponent } from './view-queries/view-queries.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CounterComponent, EffectsComponent, EqualityComponent, UntrackedComponent, CleanupComponent, RxjsInteropComponent, ToObservableComponent, SignalInputsComponent, ModelInputsComponent],
+  imports: [CounterComponent, EffectsComponent, EqualityComponent, UntrackedComponent, CleanupComponent, RxjsInteropComponent, ToObservableComponent,
+    SignalInputsComponent, ModelInputsComponent, ViewQueriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'signals';
 
-  parentChecked = true;
+  // `cmp` is a Signal (Signal<ViewQueriesComponent | undefined>) that holds a reference to a child component
+  cmp = viewChild(ViewQueriesComponent);
+  el = viewChild('el'); // `el` is a Signal that holds a reference to an element in the own template 
+
+  constructor() {
+    effect(() => {
+      console.log("cmp: ",this.cmp(), " el: ", this.el());
+    });
+  }
+
+  // parentChecked = true;
 
   // handleCheckChange(value: boolean) {
   //   console.log("parent handle checked change", value);
---
+++ signals/src/app/view-queries/view-queries.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-queries',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-queries.component.html',
+  styleUrl: './view-queries.component.css'
+})
+export class ViewQueriesComponent {
+
+  age = 35;
+
+}
