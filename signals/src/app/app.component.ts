c396773fd89b9faad8bd466013beecc32377ca3c
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-29 09:55:39 +0530

    model inputs without [()]

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -7,16 +7,24 @@ import { CleanupComponent } from './cleanup/cleanup.component';
 import { RxjsInteropComponent } from './rxjs-interop/rxjs-interop.component';
 import { ToObservableComponent } from './to-observable/to-observable.component';
 import { SignalInputsComponent } from './signal-inputs/signal-inputs.component';
+import { ModelInputsComponent } from './model-inputs/model-inputs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CounterComponent, EffectsComponent, EqualityComponent, UntrackedComponent, CleanupComponent, RxjsInteropComponent, ToObservableComponent, SignalInputsComponent],
+  imports: [CounterComponent, EffectsComponent, EqualityComponent, UntrackedComponent, CleanupComponent, RxjsInteropComponent, ToObservableComponent, SignalInputsComponent, ModelInputsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'signals';
+
+  parentChecked = true;
+
+  handleCheckChange(value: boolean) {
+    console.log("parent handle checked change", value);
+    this.parentChecked = value;
+  }
 
   // showCleanupComponent = signal(false);
 
