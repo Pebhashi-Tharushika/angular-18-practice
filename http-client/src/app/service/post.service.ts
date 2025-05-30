af27dc2fe81ed6246b4e9f278db387d057864c56
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-05-30 11:44:41 +0530

    GET request

--- http-client/src/app/service/post.service.ts
+++ http-client/src/app/service/post.service.ts
@@ -6,5 +6,19 @@ import { Injectable } from '@angular/core';
 })
 export class PostService {
 
+  private apiUrl = "https://jsonplaceholder.typicode.com/posts";
+
   constructor(private httpClient: HttpClient) { }
+
+  // return type: Observable<Post[]>
+  getPosts() {
+    return this.httpClient.get<Post[]>(this.apiUrl);
+  }
+}
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
