b4196a54a47ef8cf3c226c7d1ad778eee6f6f139
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-06-01 11:30:01 +0530

    functional and DI-based interceptors are used simultaneously

--- http-client/src/app/app.config.ts
+++ http-client/src/app/app.config.ts
@@ -2,17 +2,20 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+// import { loggingInterceptor } from './interceptors/logging.interceptor';
+import { customHeaderInnterceptor } from './interceptors/custom-header.interceptor';
+import { responseLoggingInterceptor } from './interceptors/response-logging.interceptor';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptors([customHeaderInnterceptor,responseLoggingInterceptor]), withInterceptorsFromDi()),
     {
-      provide: HTTP_INTERCEPTORS,
-      useClass: LoggingInterceptor,
-      multi: true
-    }
+      provide:HTTP_INTERCEPTORS,
+      useClass:LoggingInterceptor,
+      multi:true
+  }
   ]
 };
