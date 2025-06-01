11f027ba01dd07626d0e3d52647dc3594ed03d68
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-06-01 09:13:39 +0530

    add custom request headers via setHeaders and headers

--- http-client/src/app/interceptors/custom-header.interceptor.ts
+++ http-client/src/app/interceptors/custom-header.interceptor.ts
@@ -5,7 +5,8 @@ export const customHeaderInnterceptor : HttpInterceptorFn = (req,next) => {
         setHeaders:{
             'custom-header-1': 'custom-header-value-1',
             'custom-header-2': 'custom-header-value-2'
-        }
+        },
+        headers: req.headers.set('custom-header','custom-header-value')
     });
     return next(modifiedRequest);
 }
