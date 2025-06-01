debb110b79cd547371d44966e377af416ac51adc
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-06-01 10:10:48 +0530

    DI-based interceptors

--- http-client/src/app/app.config.ts
+++ http-client/src/app/app.config.ts
@@ -1,15 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { loggingInterceptor } from './interceptors/logging.interceptor';
-import { customHeaderInnterceptor } from './interceptors/custom-header.interceptor';
-import { responseLoggingInterceptor } from './interceptors/response-logging.interceptor';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([customHeaderInnterceptor, loggingInterceptor, responseLoggingInterceptor]))
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptor,
+      multi: true
+    }
   ]
 };
--- http-client/src/app/interceptors/logging.interceptor.ts
+++ http-client/src/app/interceptors/logging.interceptor.ts
@@ -1,7 +1,25 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+/* ---------------------- Functional interceptor ---------------------- */
 
-export const loggingInterceptor:HttpInterceptorFn = (req, next)=>{
-    console.log(`Request ${JSON.stringify(req)}`);
-    console.log(`Requesting ${req.url}`);
-    return next(req);
+// import { HttpInterceptorFn } from "@angular/common/http";
+
+// export const loggingInterceptor:HttpInterceptorFn = (req, next)=>{
+//     console.log(`Request ${JSON.stringify(req)}`);
+//     console.log(`Requesting ${req.url}`);
+//     return next(req);
+// }
+
+
+/* ---------------------- DI-based interceptor ---------------------- */
+
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log(`Request ${JSON.stringify(req)}`);
+        console.log(`Requesting ${req.url}`);
+        return next.handle(req);
+    }
 }
