8a1afec88877ee8e568dcc0011af92aca9ae88a5
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-06-02 15:18:28 +0530

    fetch json data

--- http-client/src/app/app.component.ts
+++ http-client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PostListComponent } from "./post-list/post-list.component";
 import { RouterOutlet } from '@angular/router';
+import { DataFetcherComponent } from "./data-fetcher/data-fetcher.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PostListComponent, RouterOutlet],
+  imports: [PostListComponent, RouterOutlet, DataFetcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ http-client/src/app/data-fetcher/data-fetcher.component.ts
@@ -0,0 +1,25 @@
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-data-fetcher',
+  standalone: true,
+  imports: [JsonPipe],
+  templateUrl: './data-fetcher.component.html',
+  styleUrl: './data-fetcher.component.css'
+})
+export class DataFetcherComponent {
+
+  private httpClient = inject(HttpClient);
+
+  jsonData: any;
+
+  getJsonData() {
+    this.httpClient.get("https://jsonplaceholder.typicode.com/users/1").subscribe(data => {
+      console.log(data)
+      this.jsonData = data;
+    });
+  }
+
+}
