26b77f952ae9846918a89b7cbd966229bb30e29f
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-06-02 11:54:21 +0530

    multiple DI-based interceptors and their orders

--- http-client/src/app/interceptors/custom-header.interceptor.ts
+++ http-client/src/app/interceptors/custom-header.interceptor.ts
@@ -1,12 +1,33 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+/* ---------------------- Functional interceptor ---------------------- */
 
-export const customHeaderInnterceptor : HttpInterceptorFn = (req,next) => {
-    const modifiedRequest = req.clone({
-        setHeaders:{
-            'custom-header-1': 'custom-header-value-1',
-            'custom-header-2': 'custom-header-value-2'
-        },
-        headers: req.headers.set('custom-header','custom-header-value')
-    });
-    return next(modifiedRequest);
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+// import { HttpInterceptorFn } from "@angular/common/http";
+
+// export const customHeaderInnterceptor : HttpInterceptorFn = (req,next) => {
+//     const modifiedRequest = req.clone({
+//         setHeaders:{
+//             'custom-header-1': 'custom-header-value-1',
+//             'custom-header-2': 'custom-header-value-2'
+//         },
+//         headers: req.headers.set('custom-header','custom-header-value')
+//     });
+//     return next(modifiedRequest);
+// }
+
+
+/* ---------------------- DI-based interceptor ---------------------- */
+
+export class CustomHeaderInnterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const modifiedRequest = req.clone({
+            setHeaders: {
+                'custom-header-1': 'custom-header-value-1',
+                'custom-header-2': 'custom-header-value-2'
+            },
+            headers: req.headers.set('custom-header', 'custom-header-value')
+        });
+        return next.handle(modifiedRequest);
+    }
 }
