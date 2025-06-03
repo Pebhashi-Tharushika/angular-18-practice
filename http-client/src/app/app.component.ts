a85b70f00831191594d06c4160ddceb225a3ca0f
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-06-03 13:10:54 +0530

    query parameter with HttpClient

--- http-client/src/app/app.component.ts
+++ http-client/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { PostListComponent } from "./post-list/post-list.component";
 import { RouterOutlet } from '@angular/router';
 import { DataFetcherComponent } from "./data-fetcher/data-fetcher.component";
+import { QueryParamComponent } from "./query-param/query-param.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PostListComponent, RouterOutlet, DataFetcherComponent],
+  imports: [PostListComponent, RouterOutlet, DataFetcherComponent, QueryParamComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ http-client/src/app/query-param/query-param.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-query-param',
+  standalone: true,
+  imports: [],
+  templateUrl: './query-param.component.html',
+  styleUrl: './query-param.component.css'
+})
+export class QueryParamComponent implements OnInit {
+
+  httpClient = inject(HttpClient);
+
+  ngOnInit(): void {
+
+    // let params = { userId: '1', id: '1' };
+
+    // this.httpClient.get('https://jsonplaceholder.typicode.com/posts', { params: params }).subscribe(data => {
+    //   console.log(data);
+    // });
+
+    const httpParams = new HttpParams();
+    httpParams.set('userId', '1').set('id', '1');
+    this.httpClient.get('https://jsonplaceholder.typicode.com/posts', { params: httpParams }).subscribe(data => {
+      console.log(data);
+    });
+
+  }
+}
